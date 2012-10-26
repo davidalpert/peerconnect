7667f52f0b9cc60e2b7ab72def883487067f06a1
Amir Barylko <amir@barylko.com>
2012-10-25 23:58:24 -0500

    List of users working|

---
+++ src/PeerCentral.WebClient/Configuration/NinjectWebClientModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Ninject.Modules;
+using PeerCentral.Domain;
+using PeerCentral.WebClient.Models;
+
+namespace PeerCentral.WebClient.Configuration
+{
+    public class NinjectWebClientModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IRepository<User>>().To<FakeUserRepository>();
+        }
+    }
+
+    public class FakeUserRepository : IRepository<User>
+    {
+        public IQueryable<User> All()
+        {
+            return new[]
+                       {
+                           new User {Id = 1, Name = "Joe"},
+                           new User {Id = 2, Name = "Anne"},
+                           new User {Id = 3, Name = "Admin"}
+                       }.AsQueryable();
+        }
+    }
+}
